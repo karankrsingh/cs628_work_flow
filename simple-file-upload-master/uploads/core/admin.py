0e5af12dfd5a21c2fed52ad5dd2a2cf985c56748
karankrsingh <karankr.singh0809@gmail.com>
2019-04-24 21:27:44 +0530

    Add files via upload

---
+++ simple-file-upload-master/uploads/core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Document
+from .models import Teacher
+from .models import Students
+admin.site.register(Document)
+admin.site.register(Teacher)
+admin.site.register(Students)
+# Register your models here.
---
+++ simple-file-upload-master/uploads/core/models.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Document(models.Model):
+    description = models.CharField(max_length=255, blank=True)
+    document = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+class Teacher(models.Model):
+    teacher_name=models.CharField(max_length=60,blank=False)
+
+class Students(models.Model):
+    std_teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    student_roll = models.CharField(max_length=60, primary_key=True)
+    student_name = models.CharField(max_length=60, blank=False)
+    topic = models.CharField(max_length=255, blank=False)
+
+    
